8ececa220ed56eb765f80025f4d5af0796809fdc
Gregg <greggubben@gmail.com>
2022-03-15 10:44:36 -0400

    day23

--- day23/amphipod.py
+++ day23/amphipod.py
@@ -2,6 +2,7 @@
 # Adavent of Code Template
 #
 import sys
+from heapq import heappush, heappop
 
 # Global Variables
 
@@ -29,34 +30,31 @@ class color:
 
 '''
 
-energy = {"A":1, "B":10, "C":100, "D":1000}
+ENERGY = {"A":1, "B":10, "C":100, "D":1000}
+HALLSTOPS = [0,1,3,5,7,9,10]
+HALLLENGTH = 11
+ENTRANCES = {"A":2, "B":4, "C":6, "D":8}
+ROOMS = 4
+roomDepth = 2
+amphipodTypes = ["A", "B", "C", "D"]
 
-spaces = {(1,1):".", (1,2):".", (1,3):".", (1,4):".", (1,5):".", (1,6):".", (1,7):".", (1,8):".", (1,9):".", (1,10):".", (1,11):".", \
-        (2,3):".",            (2,5):".",            (2,7):".",            (2,9):".", \
-        (3,3):".",            (3,5):".",            (3,7):".",            (3,9):"."}
 
 #
 # Load the file into a data array
 #
 def loadData(filename):
-    global spaces
 
     lines = []
+    burrow = ""
 
-    r = 0
-    f = open(filename)
-    for line in f:
-        #line = line.strip()
-        if r in [2,3]:
-            for c in range(3,11,2):
-                spaces[(r,c)] = line[c]
-        r += 1
-
-        lines.append(line)
+    f = open(filename,"r")
+    for c in f.read():
+        if c in "ABCD.":
+            burrow += c
 
     f.close()
 
-    return lines
+    return burrow
 
 
 #
@@ -71,29 +69,144 @@ def printLines(lines):
 #
 # Print Burrow
 #
-def printBurrow():
-    global spaces
+def printBurrow(burrow):
 
-    print("#############")
+    burrowString = "#############\n#"
 
-    rowStr = "#"
-    for i in range(1,12):
-        rowStr += color.CYAN + spaces[(1,i)] + color.END
-    rowStr += "#"
-    print(rowStr)
+    for p in range(len(burrow)):
+        occupant = burrow[p]
+        burrowString += (color.CYAN if occupant == "." else color.YELLOW) + occupant + color.END
+        if p >= HALLLENGTH:
+            burrowString += "#"
+        if p == HALLLENGTH-1:
+            burrowString += "#\n###"
+        elif p == HALLLENGTH+ROOMS-1:
+            burrowString += "##\n  #"
+        elif p>=HALLLENGTH and (p-HALLLENGTH+1)%ROOMS == 0:
+            burrowString += "  \n  #"
 
-    rowStr = "###"
-    for i in range(3,10,2):
-        rowStr += color.CYAN + spaces[(2,i)] + color.END + "#"
-    rowStr += "##"
-    print(rowStr)
+    burrowString += "########"
 
-    rowStr = "  #"
-    for i in range(3,10,2):
-        rowStr += color.CYAN + spaces[(3,i)] + color.END + "#"
-    print(rowStr)
+    print(burrowString)
 
-    print("  #########  ")
+
+#
+# Print the changes in the burrow (paths) to the solution
+#
+def printPath(path):
+    for burrow in path:
+        print()
+        printBurrow(burrow)
+
+
+#
+# See if the path is clear
+#
+def pathClear(startPos, endPos, burrow):
+    step = 1 if startPos < endPos else -1
+    for pos in range(startPos+step, endPos+step, step):
+        if burrow[pos] != ".":
+            return False
+    return True
+
+
+#
+# Get hallway possibilities
+#
+def getHallPositions(entrancePos, burrow):
+    for hallPos in [pos for pos in HALLSTOPS if burrow[pos] == "."]:
+        if pathClear(entrancePos, hallPos, burrow):
+            yield hallPos
+
+
+#
+# See if the Amphipod can leave its room
+#
+def canLeaveRoom(burrow, roomPoses):
+    for roomPos in roomPoses:
+        if burrow[roomPos] == ".":
+            continue
+        return roomPos, True
+
+    return -1, False
+
+
+#
+# See if the Amphipod can enter its room
+#
+def canEnterRoom(startPos, amphipod, burrow, roomPos):
+    entrancePos = ENTRANCES[amphipod]
+    bestPos = -1
+    for pos in roomPos:
+        if burrow[pos] == ".":
+            bestPos = pos
+        elif burrow[pos] != amphipod:
+            return -1, False
+    if pathClear(startPos, entrancePos, burrow):
+        return bestPos, True
+    else:
+        return -1, False
+
+
+#
+# move the amphipod
+#
+def move(pos1, pos2, burrow):
+    b = list(burrow)
+    b[pos1], b[pos2] = b[pos2], b[pos1]
+    return "".join(b)
+
+
+#
+# Get the possible moves
+#
+def getPossibleMoves(burrow, target):
+    # handle Hallway to Room first - can only go into a room
+    for startPos in [pos for pos in HALLSTOPS if burrow[pos] != "."]:
+        a = burrow[startPos]
+        endPos, canEnter = canEnterRoom(startPos, a, burrow, target[a])
+        if canEnter:
+            yield startPos, endPos
+
+    # handle Room to Hallway
+    for room in "ABCD":
+        startPos, canLeave = canLeaveRoom(burrow,target[room])
+        if canLeave:
+            for endPos in getHallPositions(ENTRANCES[room], burrow):
+                yield startPos, endPos
+
+
+#
+# Find the least amount of energy to get amphipods into their rooms
+#
+def findLeastEnergy(burrow):
+    target = {r: [p for p in range(HALLLENGTH+n,len(burrow),ROOMS)] for n,r in enumerate('ABCD')}
+    print("target\n", target)
+    targetI = {v:key for key,val in target.items() for v in val}
+    print("targetI\n", targetI)
+    solution = "."*HALLLENGTH + "ABCD"*((len(burrow)-HALLLENGTH)//ROOMS)
+    print("solution\n", solution)
+    print("burrow\n", burrow)
+
+    stuffToEvaluate = [(0,burrow,[burrow])]
+    evaluated = {burrow:0}
+
+    while stuffToEvaluate:
+        evalCost, evalState, evalPath = heappop(stuffToEvaluate)
+        if evalState == solution: return evalCost, evalPath   # Least expensive found
+        for startPos, endPos in getPossibleMoves(evalState, target):
+            hallPos, roomPos = (startPos, endPos) if startPos < endPos else (endPos, startPos)
+            entrancePos = ENTRANCES[targetI[roomPos]]
+            hallDistance = abs(entrancePos - hallPos)
+            roomDistance = (roomPos - HALLLENGTH)//4 + 1
+            distance = hallDistance + roomDistance
+            newCost = evalCost + distance * ENERGY[evalState[startPos]]
+            newState = move(startPos, endPos, evalState)
+            newPath = evalPath.copy()
+            newPath.append(newState)
+            if evaluated.get(newState, 999999) > newCost:
+                evaluated[newState] = newCost
+                heappush(stuffToEvaluate,(newCost, newState, newPath))
 
 
 #
@@ -110,14 +223,15 @@ def main():
     print()
 
     # Load data
-    lines = loadData(filename)
-    print(" Lines Read: ", len(lines))
+    burrow = loadData(filename)
+    print(" Characters Read: ", len(burrow))
     print()
 
     # Do Part 1 work
-    printLines(lines)
-    printBurrow()
-    answer = "X"
+    #printLines(lines)
+    printBurrow(burrow)
+    answer, path = findLeastEnergy(burrow)
+    printPath(path)
     print()
     print("{}Answer: {}{}{}".format(color.CYAN, color.YELLOW, answer, color.END))
 
